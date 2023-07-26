05d6fa9d00372af99a157ef921e1ffdf241f36e0
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-07-25 20:43:31 -0700

    initial support for hillside52

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
+#define MIRYOKU_NAV=VI
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR

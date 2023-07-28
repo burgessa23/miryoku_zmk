75ab043efc551925c227ac7db24803929faeb074
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-07-27 17:06:40 -0700

    !!!

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -84,6 +84,6 @@ ZMK_BEHAVIOR(shift_space, hold_tap,
 )\
 &kp ESC  K00  K01  K02  K03  K04                                                         K05  K06           K07  K08  K09  XXX  \
 XXX         K10  K11  K12  K13  K14                                                         K15  K16           K17  K18  K19  &kp SEMI  \
-XXX         K20  K21  K22  K23  K24  &kp RCMD               &kp LS(LG(SPACE))  K25  K26           K27  K28  K29  &kp LSHFT  \
-&kp LG(F6)  &kp LS(LG(A))     &kp LA(C)  &kp F11   K32  U_LT(U_NAV, &shift_space)                    K34     U_LT(U_NUM, BSPC)  U_LT(U_SYM, RET)                K37  &kp F12  &kp LG(LBKT) XXX &kp LG(RBKT) \
+&kp LSHFT        K20  K21  K22  K23  K24  &kp RCMD               &kp LS(LG(SPACE))  K25  K26           K27  K28  K29  &kp RSHFT  \
+&kp LG(F6)  &kp LS(LG(A))     &kp LA(C)  &kp F11   K32  U_LT(U_NAV, &shift_space LSHIFT SPACE)                    K34     U_LT(U_NUM, BSPC)  U_LT(U_SYM, RET)                K37  &kp F12  &kp LG(LBKT) XXX &kp LG(RBKT) \
 

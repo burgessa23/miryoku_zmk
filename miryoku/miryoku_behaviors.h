ad6f96cfac3c72f5ccc9a2bd4ff7fa57865a8b76
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-07-30 12:58:57 -0700

    maybe?

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -8,4 +8,5 @@
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_HML(MOD, TAP) &u_hml MOD TAP
 #define U_HMR(MOD, TAP) &u_hmr MOD TAP
+#define U_BAK_FWD_PIPE &u_back_fwd
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP

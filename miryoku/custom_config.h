fb972ecf0892487c0fc1cb4d69cebecca1a9c168
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-08-10 13:59:42 -0700

    cmon

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -56,6 +56,6 @@ nice_view_spi: &spi0 {
 )\
 XXX  K00  K01  K02  K03  K04                                                         K05  K06           K07  K08  K09  XXX  \
 XXX  K10  K11  K12  K13  K14                                                         K15  K16           K17  K18  K19  XXX  \
-XXX  K20  K21  K22  K23  K24  U_HML(RCMD, LG(PIPE))              &U_RAY  K25  K26           K27  K28  K29  XXX  \
+XXX  K20  K21  K22  K23  K24  U_HML(RCMD, LG(PIPE))              &u_ray  K25  K26           K27  K28  K29  XXX  \
 XXX  &kp LS(LG(A))     &kp LS(LA(C))  U_HML(LSHFT, F11)   K32  K33                    K34     K35  K36                K37  U_HMR(RSHFT, F12)  &kp LG(LBKT) &kp LG(RBKT) XXX  \
 

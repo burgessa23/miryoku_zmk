8ca1a38bd183557fa5c04746edb6270da8985a98
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-08-10 13:39:37 -0700

    try raw

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -239,7 +239,7 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp BSPC,           &kp RET,          &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_TAP_QWERTZ \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
@@ -384,7 +384,7 @@ U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp 
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_UND,             &kp LC(LEFT),      &kp LC(DOWN),      &kp LC(UP),      &kp LC(RIGHT),        \
-U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+U_NP,              U_NP,              U_BTN3,            U_BTN2,            U_BTN1,            U_BTN1,            U_BTN2,            U_BTN3,            U_NP,              U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_CLEAR \
--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -10,7 +10,7 @@
 #define U_HMR(MOD, TAP) &u_hmr MOD TAP
 #define U_BAK_FWD_PIPE &u_back_fwd
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
-#define U_RAY(TIP, TAP) &u_ray 0 0
+#define U_RAY &u_ray
 #define U_SHFT_SPC_NAV(MOD, TAP) &shift_space MOD 0
 #define U_SHFT_ESC_MEDIA(MOD, TAP) &shift_esc MOD 0
 #define U_HT2(MOD, TAP) &ht2 MOD TAP

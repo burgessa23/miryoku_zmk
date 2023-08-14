838410a2a520a3b1cf6a8214ff943c7f05c889cb
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-08-13 19:52:40 -0700

    again

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -11,6 +11,7 @@
 #define U_BAK_FWD_PIPE &u_back_fwd
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
 #define U_RAY &u_ray
+#define U_ZOOM &u_zoom
 #define U_SHFT_SPC_NAV(MOD, TAP) &shift_space MOD 0
 #define U_SHFT_ESC_MEDIA(MOD, TAP) &shift_esc MOD 0
 #define U_HT2(MOD, TAP) &ht2 MOD TAP

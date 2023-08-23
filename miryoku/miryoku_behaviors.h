a1df00b9291195acb9e17b59898e405d21237a52
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-08-23 08:57:32 -0700

    MOVE THINGS AROUND

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -12,6 +12,7 @@
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
 #define U_RAY &u_ray
 #define U_RAY_WRAP(MOD, TAP) &ray_wrap MOD 0
+#define U_ESC_CAPS_WRAP(MOD, TAP) &esc_caps_wrap MOD 0
 #define U_ZOOM &u_zoom
 #define U_SHFT_SPC_NAV(MOD, TAP) &s_spc 0 0 
 #define U_SHFT_ESC_MEDIA(MOD, TAP) &shift_esc MOD 0

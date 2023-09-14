e744d819be13fe4c9a4c561a48c2efaf8cbf48bd
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-09-14 14:21:01 -0700

    i meant this

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -117,7 +117,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 AS(Q),             AS(W),          AS(E),                         AS(R),                 AS(T),              AS(Y),             AS(U),           AS(I),            AS(O),             AS(P),             \
 U_HML(LGUI, A),    U_HML(LALT, S), U_HML(LCTRL, D),               U_HML(LSHFT, F),       &kp G,              &kp H,             U_HMR(LSHFT, J), U_HMR(LCTRL, K),  U_HMR(LALT, L),    U_HMR(LGUI, SQT),   \
-AS(Z), AS(X), AS(C),                         AS(V,                 AS(B,              AS(N,             AS(M,           &kp COMMA,        U_HMR(RALT, DOT),  U_LT(U_BUTTON, SLASH),\
+AS(Z), AS(X), AS(C),                         &kp V,                 &kp B,              &kp N,             &kp M,           &kp COMMA,        U_HMR(RALT, DOT),  U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,           U_MED_CAPS_WORD(U_MEDIA, ESC), U_LT(U_NAV, SPACE),   U_LT(U_MOUSE, TAB), U_LT(U_SYM, BSPC), U_LT(U_NUM, RET), U_LT(U_FUN, DEL), U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTZ \

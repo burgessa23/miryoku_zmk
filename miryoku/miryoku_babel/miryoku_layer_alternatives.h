2eeecc3562020f8dd925d3fc4b67d127a49d175a
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-10-02 12:40:51 -0700

    word and line bounds

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -273,10 +273,10 @@ U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp BSPC,           &kp RET,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_NAV \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
-U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp LA(LEFT),      &kp PG_DN,         &kp PG_UP,         &kp LA(RIGHT),           \
-U_NP,              U_NP,              U_NA,              &tog U_NAV,              U_NA,              &kp BSPC,          &kp RET,           U_NP,              &kp DEL,           U_NP
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,                      U_CPY,       U_CUT,     U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,                   &kp DOWN,    &kp UP,    &kp RIGHT,         \
+U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           U_MT(LG(LEFT), LA(LEFT)),   &kp PG_DN,   &kp PG_UP, U_MT(LG(RIGHT), LA(RIGHT)),           \
+U_NP,              U_NP,              U_NA,              &tog U_NAV,        U_NA,              &kp BSPC,          &kp RET,                    U_NP,        &kp DEL,   U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_MOUSE_INVERTEDT_FLIP \

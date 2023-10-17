e6e1a507d151b5282b711c41e3cdeb12f5b7304f
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-10-16 23:15:34 -0700

    add stuff for hillside 46

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -48,15 +48,20 @@ nice_view_spi: &spi0 {
     cs-gpios = <&gpio1 1 GPIO_ACTIVE_HIGH>;
 };
 
-#define MIRYOKU_LAYOUTMAPPING_HILLSIDE52(\
+#define MIRYOKU_LAYOUTMAPPING_HILLSIDE46(\
      K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
 )\
-XXX  K00     K01                       K02  K03               K04                                                      K05  K06               K07  K08           K09          XXX  \
-XXX  K10     K11                       K12  K13               K14                                                      K15  K16               K17  K18           K19          XXX  \
-XXX  K20     K21                       K22  K23               K24  U_HML(LS(LA(C)), ESC)             U_HMR(LA(TAB), LS(LG(SPACE)))  K25  K26               K27  K28           K29          XXX  \
-XXX  U_ZOOM  U_HML(RCMD, LG(PIPE))  K32  U_MT(LCMD, F11)  K33  K34                           K35                   K36  U_HMR(LALT, F12)  K37  &kp LG(LBKT)  U_LT(U_BUTTON, LG(RBKT)) XXX  \
+&kp ESC                K00  K01  K02  K03  K04                                                                                  K05               K06  K07  K08  K09  &kp BSPC     \
+&kp LSHFT              K10  K11  K12  K13  K14                                                                                  K15               K16  K17  K18  K19  &kp RSHFT    \
+U_LT(U_BUTTON,U_ZOOM)  K20  K21  K22  K23  K24             U_HML(LS(LA(C)), ESC)                 U_HMR(LA(TAB), LS(LG(SPACE)))  K25               K26  K27  K28  K29  &u_back_fwd  \
+                                      K32  U_MT(LCMD, F11) K33                    K34       K35  K36                            U_HMR(LALT, F12)  K37                              \
 
 
+
+// XXX  K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  XXX  \
+// XXX  K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX  \
+// XXX  K20  K21  K22  K23  K24  XXX                 XXX  K25  K26  K27  K28  K29  XXX  \
+//                     XXX  K32  K33  K34       K35  K36  K37  XXX                      \

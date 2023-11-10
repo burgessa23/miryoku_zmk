4f8042a439ad8d3e2ddbcdc92bb7bfb8b4a0ba09
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2023-11-10 12:21:29 -0800

    button layer ccp to match nav

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -381,7 +381,7 @@ U_NP,             U_NP,              &kp K_APP,         &kp SPACE,         &kp T
 
 
 #define MIRYOKU_ALTERNATIVES_BUTTON \
-U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_UND,             &kp LC(LEFT),      &kp LC(DOWN),      &kp LC(UP),      &kp LC(RIGHT),        \
 U_NP,              U_NP,              U_BTN3,            U_BTN2,            U_BTN1,            U_BTN1,            U_BTN2,            U_BTN3,            U_NP,              U_NP

7f465ae00915b7fe73d14a7154bce1cfaa923fe1
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2024-01-02 11:30:33 -0800

    fix mouse buttons

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -295,7 +295,7 @@ U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BT
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_NU,              U_WH_L,            U_MS_U,            U_WH_R,            U_WH_U,            \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_NU,              U_MS_L,            U_MS_D,            U_MS_R,            U_WH_D,            \
 U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN1,            U_BTN2,            U_BTN3,            U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE_VI \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
@@ -379,14 +379,6 @@ U_NP,         U_NP,              &kp UNDER,          &kp RPAR,         U_NA,    
 // &kp LGUI,     &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp PLUS,      &kp UNDER,    &kp LSHFT,         &kp LCTRL,         &kp LALT,         &kp LGUI,          \
 // U_NA,         &kp RCMD,          &u_to_U_NUM,       &u_to_U_BASE,      &kp LBRC,      &kp RBRC,     &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,         U_NA,              \
 // U_NP,         U_NP,              U_NA,              &kp UNDER,         U_NA,          U_NA,         &kp BSLH,          U_NA,              U_NP,             U_NP
-
-
-#define MIRYOKU_ALTERNATIVES_FUN_FLIP \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp PSCRN,         &kp F7,            &kp F8,            &kp F9,            &kp F12,           \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp SLCK,          &kp F4,            &kp F5,            &kp F6,            &kp F11,           \
-U_NA,              &kp RALT,          &u_to_U_MEDIA,     &u_to_U_FUN,       U_NA,              &kp PAUSE_BREAK,   &kp F1,            &kp F2,            &kp F3,            &kp F10,           \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp TAB,           &kp SPACE,         &kp K_APP,         U_NP,              U_NP
-
 #define MIRYOKU_ALTERNATIVES_FUN \
 &kp F11,           &kp F7,            &kp F8,            &kp F9,            U_NA,           U_UND,            U_CUT,         U_CPY,          U_PST,        U_RDO,            \
 &kp F12,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,       &kp LNLCK,        &kp LSHFT,     &kp LCTRL,      &kp LALT,     &kp LGUI,          \

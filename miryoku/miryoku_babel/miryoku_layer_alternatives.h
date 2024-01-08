0b1e03ae04833c6c380a1ec505298e0fe1c80a3d
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2024-01-07 19:25:45 -0800

    found it

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -329,7 +329,7 @@ U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         U_RG
 U_NP,              U_NP,              &kp C_MUTE,        &kp C_PP,          &kp C_STOP,        U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MEDIA_INVERTEDT \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_NA,              &kp F15,         &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG         \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_NA,              &kp F15,         &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG,         \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_NA,              &kp F14,         &kp C_VOL_DN,      &kp C_PREV,        U_NA,            \
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_out_tog,        &u_bt_sel_0,     &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,     \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,        &kp C_MUTE,        U_NP,              U_NP

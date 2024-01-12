dee6fd12670ddf827736ee24f3a49d5689639dfc
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2024-01-11 20:27:02 -0800

    fix it

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -66,7 +66,7 @@ nice_view_spi: &spi0 {
 )\
 U_LT(U_BUTTON, ESC)          K00   K01   K02   K03  K04                                                                           K05  K06              K07  K08  K09  U_LT(U_BUTTON, DEL)      \
 U_SHFT_CAPS(LSHFT, LS(TAB))  K10   K11   K12   K13  K14                                                                           K15  K16              K17  K18  K19  U_SHFT_NUM(RSHFT)        \
-U_BP_WRAP(LG(LC(LALT)))      K20   K21   K22   K23  K24             U_HML(LCTRL, LS(LA(C)))                   U_MT(LA(TAB), LS(LG(SPACE)))   K25  K26   K27  K28  K29  U_FP_WRAP(LG(LC(LALT)))  \
+U_BP_WRAP(LG(LC(LALT)))      K20   K21   K22   K23  K24             U_HML(LCTRL, LS(LA(C)))                   U_RAY_WRAP(RCTRL)   K25  K26              K27  K28  K29  U_FP_WRAP(LG(LC(LALT)))  \
                                                K32  U_MT(LCMD, F11)                         K33   K34   K35                       K36  U_MT(LALT, F12)  K37                                     \
 
 

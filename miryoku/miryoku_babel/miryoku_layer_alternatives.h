d49b4ce8517b6ffca98998ee20865d3dc9cb6a77
axburgess-godaddy <63809464+axburgess-godaddy@users.noreply.github.com>
2024-01-11 20:39:16 -0800

    ???

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -300,7 +300,7 @@ U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BT
 #define MIRYOKU_ALTERNATIVES_MOUSE_INVERTEDT \
 U_BOOT,            U_CUT,             U_CPY,             U_PST,             U_NA,              U_NU,              U_WH_L,            U_MS_U,            U_WH_R,            U_WH_U,            \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_NU,              U_MS_L,            U_MS_D,            U_MS_R,            U_WH_D,            \
-U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_BTN4,            U_BTN1,            U_BTN3,            U_BTN2,            U_UND,             \
+U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_MOUSE,     U_NA,              U_NA,              U_BTN1,            U_BTN3,            U_BTN2,            U_UND,             \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_BTN1,            U_BTN3,            U_BTN2,            U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE_VI \
